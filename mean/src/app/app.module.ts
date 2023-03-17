09ddaa614db9c4cf688b8b7cb576a2d929b189dc
Gopi Krishna T <gopi.krishna@samco.in>
2023-03-17 20:30:44 +0530

    new components
    creating new components

--- mean/src/app/app.module.ts
+++ mean/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatepostComponent } from './createpost/createpost.component';
+import { PostlistComponent } from './postlist/postlist.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatepostComponent,
+    PostlistComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
