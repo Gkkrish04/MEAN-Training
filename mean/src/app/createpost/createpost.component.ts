a96ad4ee5ff7a6140a5dca7ed2285ef00a0e190d
Gopi Krishna T <gopi.krishna@samco.in>
2023-03-17 22:43:12 +0530

    string interpolation sample
    here we do a string interpolation to bind the data to the paragraph tag with click event

--- mean/src/app/createpost/createpost.component.ts
+++ mean/src/app/createpost/createpost.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatepostComponent implements OnInit {
 
+  newPost = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddPost(){
+    this.newPost = 'This user post';
+  }
 }
