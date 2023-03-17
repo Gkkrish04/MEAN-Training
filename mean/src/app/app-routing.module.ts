33beebb6d5278047f29729bbefa3b0c2609885cf
Gopi Krishna T <gopi.krishna@samco.in>
2023-03-17 22:01:37 +0530

    routing navigation
    simple router navigation for new project

--- mean/src/app/app-routing.module.ts
+++ mean/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatepostComponent } from './createpost/createpost.component';
+import { PostlistComponent } from './postlist/postlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:CreatepostComponent},
+  {path:"createPost", component:CreatepostComponent},
+  {path:"postList", component:PostlistComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
