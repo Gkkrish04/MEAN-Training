618f59b1a4fa633b341628c493eae6488fcdb1d1
Gopi Krishna T <gopi.krishna@samco.in>
2023-03-18 14:16:30 +0530

    Angular form with validation
    using form tag for get data from all input field and also we do validation for input fields, we using the local referance tag to set default angular ngForm element, this tag give as entire html form element. this ngForm object give as all the parameters, we pass this local referance tag element to addpost() parameter.

--- mean/src/app/createpost/createpost.component.ts
+++ mean/src/app/createpost/createpost.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Post } from '../model/post.model';
 
 @Component({
@@ -18,8 +19,13 @@ export class CreatepostComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddPost(){
-    const post: Post = {title: this.enteredTitle, content: this.enteredContent};
+  onAddPost(form:NgForm){
+    if(form.invalid){
+      return;
+    }
+    const post: Post = {title: form.value.title, content: form.value.content};
     this.postCreate.emit(post);
   }
+
+  //using form tag for get data from all input field and also we do validation for input fields, we using the local referance tag to set default angular ngForm element, this tag give as entire html form element. this ngForm object give as all the parameters, we pass this local referance tag element to addpost() parameter.
 }
