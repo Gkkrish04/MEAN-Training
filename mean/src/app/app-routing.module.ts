7e994b4c693f5c3e04ebf7b4b1b30217471f1538
Gopi Krishna T <gopi.krishna@samco.in>
2023-03-29 10:23:18 +0530

    Simple router action
    Creating simple router action

--- mean/src/app/app-routing.module.ts
+++ mean/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { CreatepostComponent } from './createpost/createpost.component';
 import { PostlistComponent } from './postlist/postlist.component';
 
 const routes: Routes = [
-  {path:"", component:CreatepostComponent},
+  {path:"", component:PostlistComponent},
   {path:"createPost", component:CreatepostComponent},
   {path:"postList", component:PostlistComponent}
 ];
