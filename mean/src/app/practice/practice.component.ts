b201ddcbb894e9769a725292bbf5776298b4caa1
Gopi Krishna T <gopi.krishna@samco.in>
2023-04-06 09:00:30 +0530

    commit

--- mean/src/app/practice/practice.component.ts
+++ mean/src/app/practice/practice.component.ts
@@ -4,59 +4,74 @@ import { AuthService } from '../service/auth.service';
 @Component({
   selector: 'app-practice',
   templateUrl: './practice.component.html',
-  styleUrls: ['./practice.component.scss']
+  styleUrls: ['./practice.component.scss'],
 })
 export class PracticeComponent implements OnInit {
-
   arr1 = [
-    {id:1, name:'name1'},
-    {id:2, name:'name2'},
-    {id:3, name:'name3'},
-    {id:4, name:'name4'},
+    { id: 1, name: 'name1' },
+    { id: 2, name: 'name2' },
+    { id: 3, name: 'name3' },
+    { id: 4, name: 'name4' },
   ];
 
   arr2 = [
-    {id:5, name:'name5'},
-    {id:2, name:'name2'},
-    {id:6, name:'name6'},
-    {id:7, name:'name7'},
-    {id:5, name:'name5'},
+    { id: 5, name: 'name5' },
+    { id: 2, name: 'name2' },
+    { id: 6, name: 'name6' },
+    { id: 7, name: 'name7' },
+    { id: 5, name: 'name5' },
   ];
 
-  constructor(private authService: AuthService) { }
+  arr3 = ['money', 'currency', 'doller', 'Rupee', 'yen'];
 
-  ngOnInit(): void {
-  }
+  constructor(private authService: AuthService) {}
 
-  arrFilter(){
-    const filterArr =
-    this.arr1.filter( el=>{
-      console.log(el);
+  ngOnInit(): void {}
+
+  arrFilter() {
+    const filterArr = this.arr1.filter((el) => {
       return el.id == 3;
-    })
+    });
     return filterArr;
   }
 
-  joinArr(){
+  arrayBasics() {
+    console.log(this.arr3.join(' '));
+    console.log(this.arr3.pop());
+    console.log(this.arr3.shift());
+    console.log(this.arr3.splice(0, 1));
+    console.log(this.arr3.slice(1));
+    console.log(this.arr3.reverse());
+  }
+
+  joinArr() {
     let joinArrVal = this.arr1.concat(this.arr2);
     return joinArrVal;
   }
 
-  removeDuplicate(){
+  removeDuplicate() {
     let joinArrVal = this.arr1.concat(this.arr2);
-    let finArr = joinArrVal.filter((obj, index, self)=> {
-      return index === self.findIndex((t)=> (t.id === obj.id));
+    let finArr = joinArrVal.filter((obj, index, self) => {
+      return index === self.findIndex((t) => t.id === obj.id);
     });
     return finArr;
   }
 
-  check(){
-   let value = this.arrFilter();
-   console.log(JSON.stringify(value));
-   let joinArr = this.joinArr();
-   console.log(JSON.stringify(joinArr));
-   let finArr = this.removeDuplicate();
-   console.log( JSON.stringify(finArr));
+  printSerise(len){
+    let val = len;
+    for(let i = 1; i <= val; i++){
+      console.log(Array(i).fill('*').join(' '));
+    }
   }
 
+  check() {
+    let value = this.arrFilter();
+    console.log('Array Filter', JSON.stringify(value));
+    let joinArr = this.joinArr();
+    console.log('Join Array Two Array', JSON.stringify(joinArr));
+    let finArr = this.removeDuplicate();
+    console.log('Remove duplicate on array', JSON.stringify(finArr));
+    // this.arrayBasics();
+    this.printSerise(5);
+  }
 }
