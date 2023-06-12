1e454cfa089b9ea4d103fdfbd06ef0147c92b7f6
Gopi Krishna T <gopi.krishna@samco.in>
2023-06-12 11:02:00 +0530

    error handling

--- mean/src/app/service/error.interceptor.ts
+++ mean/src/app/service/error.interceptor.ts
@@ -7,17 +7,27 @@ import {
   HttpErrorResponse
 } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from '../dialog/error-dialog/error-dialog.component';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse)=>{
-        console.log(error);
-        alert(error.error.message);
+        console.log(error.error.message);
+        this.dialog.open(ErrorDialogComponent, {
+          panelClass: ['animate__animated', 'animate__slideInUp'],
+          width: '350px',
+          height: 'auto',
+          disableClose: false,
+          position: { right: '50%', top: '10rem' },
+          data:error.error.message,
+        })
+        // alert(error.error.message);
         return throwError(error);
       })
     );
